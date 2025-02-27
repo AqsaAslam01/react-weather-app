608d31635518407c380d921464935be0dac30034
AqsaAslam01 <aqsaaslam.78@gmail.com>
2025-02-27 21:22:16 +0500

    WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
 import './App.css'
+
+const api = {
+  key: 'ba96337e554f535cf764f571a955cb3e',
+  base: 'https://api.openweathermap.org/data/2.5/'
+}
 
 function App() {
 
   return (
-    <>
-    <h1>hello</h1>
-    </>
+    <div className='app'>
+      <main>
+        <div className='search-box'>
+          <input
+           type='text'
+           className='search-bar'
+           placeholder='search...'>
+           </input>
+        </div>
+
+      </main>
+    </div>
   )
 }
 
