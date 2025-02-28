dda24e202126e4d2c14553d2538d2ca0f67295f4
AqsaAslam01 <aqsaaslam.78@gmail.com>
2025-02-28 21:37:10 +0500

    added location and date

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,21 @@ const api = {
 }
 
 function App() {
+ const dateBuilder = (d) => {
+  let months = [
+    "January", "February", "March", "April", "May", "June", 
+    "July", "August", "September", "October", "November", "December"
+  ];
+  let days = [
+    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+  ];
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear()
 
+  return `${day} ${date} ${month} ${year}`
+ }
   return (
     <div className='app'>
       <main>
@@ -17,7 +31,10 @@ function App() {
            placeholder='search...'>
            </input>
         </div>
-
+        <div className='location-box'>
+          <div className='location'>Lahore, Pakistan</div>
+          <div className='date'>{dateBuilder(new Date())}</div>
+        </div>
       </main>
     </div>
   )
