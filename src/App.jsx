70d888229f265a29278a5e368ae82f604821f208
AqsaAslam01 <aqsaaslam.78@gmail.com>
2025-03-08 22:23:37 +0500

    setup api

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+//  const url = `https://api.openweathermap.org/data/2.5/weather?q=Lahore,PK&appid=2c60111b22cd067607932c86dd6ec2f4`
+  return (
+    <div className='app'>
+     
+    </div>
+  )
+}
+
+export default App
